60c21da5832e4f9417fded88095610dd884a78cb
denghang <denghang@huiwtech.com>
2020-09-22 21:59:20 +0800

    cloud-provider-pament8004 整合zookeeper

---
+++ cloud-provider-pament8004/src/main/java/com/dengooo/springcloud/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package com.dengooo.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/payment/zk")
+    public String paymentZk() {
+        return "zookeeper: ==========" +serverPort + UUID.randomUUID().toString();
+    }
+}
