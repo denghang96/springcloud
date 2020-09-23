2b48ccdfc7404136bec566eed2bbdf4b31e4713b
denghang <denghang@huiwtech.com>
2020-09-23 22:25:33 +0800

    cloud-consumerzk-order80 整合zookeeper单机、集群

---
+++ cloud-consumerzk-order80/src/main/java/com/dengooo/springcloud/controller/OrderZkController.java
@@ -0,0 +1,24 @@
+package com.dengooo.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderZkController {
+
+    public static final String INVOKE_URL = "http://cloud-payment-service";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/consumer/payment/zk")
+    public String payInfo() {
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/zk", String.class);
+        return result;
+    }
+}
